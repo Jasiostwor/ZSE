a06af06e4eb0ab00340a4bd4a4140ee90acfe11e
Jasiostwor <janekhorodecki@gmail.com>
2023-10-04 13:00:21 +0200

    new

---
+++ Aplikacje Mobilne/Intencje/app/src/main/java/com/example/intencje/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.intencje;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnFirtsActivity = (Button) findViewById(R.id.btnName);
+
+        btnFirtsActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
