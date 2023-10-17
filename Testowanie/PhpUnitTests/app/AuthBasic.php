ed73a3ce7aee6f42b02aa257e79b132bbda38e4b
Jasiostwor <jasiostwor1@gmail.com>
2023-10-17 22:34:33 +0200

    Wip

---
+++ Testowanie/PhpUnitTests/app/AuthBasic.php
@@ -0,0 +1,146 @@
+<?php
+
+require("libs/DataBaseConn.php");
+
+/**
+ * Klasa do autoryzacji jednorazowego dostępu do fragmentu serwisu
+ * @author Grzegorz Petri
+ * @since 0.2
+ */
+
+class AuthBasic
+{
+
+    /**
+     * @desc Generuje kod szyfrowania
+     * @param string $algo Algorytm szyfrowania
+     * @return string Kod szyfrujący
+     */
+    public function genFingerprint($algo)
+    {
+        $userAgent = $_SERVER['HTTP_USER_AGENT'];
+        $remoteAddress = $_SERVER['REMOTE_ADDR'];
+        $uniqueHash = uniqid();
+        $isSecure = true;
+    
+        $dataToHash = $userAgent . $remoteAddress . $uniqueHash . $isSecure;
+        return hash_hmac($algo, $dataToHash, 'YourSecretKey');
+    }
+
+    /**
+     * @desc Generuje kod wymagany do podania podczas autoryzacji dostępu, wg. podanych parametrów
+     * @param int $length Długość kodu - liczba znaków
+     * @param int $min Minimalna wartość dla generowanego numeru
+     * @param int $max Maksymalna wartość dla generowanego numeru
+     * @return int Zwraca wygenerowaną na podstawie parametrów liczbę, która musi zostać uzupełniana zerami, jeżeli trzeba spełnić długość
+     */
+    public function createCode($length = 6, $min = 1, $max = 999999)
+    {
+        $max = substr($max, 0, $length);
+        return str_pad(mt_rand($min, $max), $length, '0', STR_PAD_LEFT); // losowanie 1-999999
+    }
+
+
+    public function compAuthCode($emlAuth, $idzAuth, $authCode)
+    {
+    }
+    public function doAuthByEmail($person, $email)
+    {
+    }
+    public function checkIfValidReqest($person, $email)
+    {
+    }
+    private function checkIfValidReqest2f($emlAuth, $idzAuth)
+    {
+    }
+
+
+
+    /**
+     * @desc Weryfikuje kod autoryzacyjny dla autoryzacji jednorazowego dostępu.
+     * @param string $codeNo Kod autoryzacyjny do weryfikacji.
+     * @return bool True, jeśli kod autoryzacyjny jest poprawny i ważny; False, jeśli kod jest niepoprawny lub wygasł.
+     */
+    public function verifyQuickRegCode($codeNo)
+    {
+        $tbl = 'cmswebsiteauth';
+        $cols = array('email', 'authCode', 'datetime');
+        $options = array('where' => "authCode = '$codeNo'");
+    
+        $db = new DataBaseConn('localhost', 'root', '', 'authtida');
+        $db->connect();
+    
+        $data = $db->get($tbl, $cols, $options);
+    
+        $db->disconnect();
+    
+        if (count($data) === 1) {
+            $authData = $data[0];
+            $authDate = strtotime($authData['datetime']);
+            $currentTime = strtotime(date("Y-m-d H:i:s"));
+    
+            // Sprawdź, czy kod autoryzacyjny nie wygasł (np. datą ważności)
+            if ($authDate >= $currentTime) {
+                // Jeżeli kod autoryzacyjny jest nadal ważny, zwróć prawdę
+                return true;
+            }
+        }
+    
+        // Jeżeli kod autoryzacyjny jest niepoprawny lub wygasł, zwróć fałsz
+        return false;
+    }
+
+    /**
+     * @desc Tworzy wpis w BD z numerem pozwalającym na uwierzytelnienie Requesta
+     * Tworzony Token do uwierzytelnienia zapisując adres Email oraz ID użytkownika
+     * Token musi zostać wysłany na pocztę użytkownika, stąd zwracany jest Obiekt informacyjny
+     * @param string $email Adres email użytkownika do uwierzytelnienia
+     * @param int $userId Numer ID użytkownika do uwierzytelnienia
+     * @return array|false	Wygenerowany Token LUB Fałsz
+     */
+    public function createAuthToken($email, $userId)
+    {
+        $authCode = $this->createCode();
+        $authDate = date("Y-m-d H:i:s");
+
+        $addrIp = '127.0.0.1'; #TODO ->code
+        $opSys = 'Linux';#TODO @see whichBrowser
+        $browser = 'FF';#TODO @see whichBrowser
+        $fingerprint = $this->genFingerprint("sha512");
+        $session_id = "1234567891";
+
+        $content = array(
+            'addrIp' => $addrIp, 'datetime' => $authDate,
+            'email' => $email, 'authCode' => $authCode,
+            'opSystem' => $opSys, 'browser' => $browser
+        );
+
+
+        $tbl = 'cmswebsiteauth';
+        $cols = array(
+            'session_id', 'usrId', 'addrIp', 'fingerprint', 'datetime', 'email', 'authCode', 'opSystem', 'browser'
+        );
+       
+        $vals = array(
+            $session_id, $userId, $addrIp, $fingerprint, $authDate, $email, $authCode, $opSys, $browser
+        );
+
+        # START >> db->put()
+        $db = new DataBaseConn('localhost', 'root', '', 'authtida');
+        $db->connect();
+
+        $db->put($tbl,$cols,$vals);
+
+        $data = $db->get($tbl, array('addrIp','datetime','email', 'authCode', 'opSystem', 'browser'), array('where' => "session_id = $session_id"));
+        
+        $db->disconnect();
+
+        // # STOP >> db->put()
+        // return $content;
+        if(count($data) === 1 && $data[0] == $content){
+            return $content;
+        }else{
+            return false;
+        }       
+    }
+}
---
+++ Testowanie/PhpUnitTests/tests/AuthBasicTest.php
@@ -0,0 +1,110 @@
+<?php
+// załącz plik testowanej klasy - dopasuj ścieżkę do pliku zgodną z własną strukturą katalogów
+require("app/AuthBasic.php");
+// użycie wbudowanych testów
+
+use PHPUnit\Framework\TestCase;
+// nazwanie i rozszerzenie własnej klasy klasą `TestCase` zawierającą Asercje do testów 
+class AuthBasicTest extends TestCase
+{
+    private $instance;
+    private $db;
+    
+    // tutaj umieść kod testów (metody)
+    public function setUp(): void
+    {
+        $this->instance = new AuthBasic();
+
+        $this->db = new DataBaseConn('localhost', 'root', '', 'authtida');
+        $this->db->connect();
+    }
+    public function tearDown(): void
+    {
+        unset($this->instance);
+
+        $this->db->disconnect();
+    }
+
+
+
+
+    public function testCreateCode()
+    {
+        $out = $this->instance->createCode();
+        // jezeli potrzeba wyświetlić cokolwiek w widoku testu, należy użyć:
+        fwrite(STDERR, print_r($out, true));
+        $len = strlen($out);
+        $this->assertIsNumeric($out, 'Wylosowano: ' . $out);
+        $this->assertEquals(6, $len, 'Długość: ' . $len);
+
+        $out = $this->instance->createCode(4);
+        $len = strlen($out);
+        $this->assertIsNumeric($out, 'Wylosowano: ' . $out);
+        $this->assertEquals(4, $len, 'Długość: ' . $len);
+        // symulowanie wylosowania liczby o mniejszej niż oczekiwana długość, którą należy uzupełnić zerami
+        // nie można liczyć, że podczas testu zawsze wygenerujemy taką liczbą, stąd skopiowanie implementacji metody
+        $out = str_pad(1111, 6, '0', STR_PAD_LEFT);
+        $len = strlen($out);
+        $this->assertIsNumeric($out, 'Wylosowano: ' . $out);
+        $this->assertEquals(6, $len, 'Długość: ' . $len);
+    }
+
+
+
+
+
+    public function testCreateAuthToken()
+    {
+        // oczekiwana struktura tokenu z następującymi informacjami
+        // TODO: ip ops browser
+        $exp = array(
+            'addrIp' => "127.0.0.1", 'datetime' => date("Y-m-d H:i:s"),
+            'email' => "janh@testingmail.com", 'authCode' => "131313",
+            'opSystem' => "Linux", 'browser' => "FF"
+        );
+
+        // wywołanie testowanej metody z przykładowymi danymi użytkownika: email i jego IDentyfikator
+        $out = $this->instance->createAuthToken('janh@testingmail.com', 69);
+        // ponieważ generowany Token jest wartością losową - musimy go napisać wartością stałą - inaczej nie ma możliwości wykonania pomyślnie testu
+        $out['authCode'] = '131313';
+        // wywołanie testu właściwego - Asercji (założenia)
+        $this->assertEqualsCanonicalizing($exp, $out, 'Tablice są różne');
+    }
+
+
+
+
+
+
+
+
+    public function testVerifyQuickRegCodeValid()
+    {     
+        // Wstaw testowy wpis do bazy danych
+        $columns = array('session_id', 'usrId', 'addrIp', 'fingerprint', 'datetime', 'email', 'authCode', 'opSystem', 'browser');
+        $values = array('1234567890', 1, '127.0.0.1', 'testHash', date("Y-m-d H:i:s"), 'test@example.com', '123456', 'Linux', 'FF');
+
+        $this->db->put('cmswebsiteauth', $columns, $values);
+
+        // Sprawdź, czy verifyQuickRegCode zwraca true dla poprawnego kodu autoryzacyjnego
+        $this->assertTrue($this->instance->verifyQuickRegCode('123456'));
+    }
+
+    public function testVerifyQuickRegCodeExpired()
+    {
+        // Wstaw przeterminowany testowy wpis do bazy danych
+        $columns = array('session_id', 'usrId', 'addrIp', 'fingerprint', 'datetime', 'email', 'authCode', 'opSystem', 'browser');
+        $values = array('1234567891', 1, '127.0.0.1', 'testHash', '2023-01-01 12:00:00', 'test@example.com', '654321', 'Linux', 'FF');
+
+        $this->db->put('cmswebsiteauth', $columns, $values);
+
+        // Sprawdź, czy verifyQuickRegCode zwraca false dla przeterminowanego kodu autoryzacyjnego
+        $this->assertFalse($this->instance->verifyQuickRegCode('654321'));
+    }
+
+    public function testVerifyQuickRegCodeNonExistent()
+    {
+        // Sprawdź, czy verifyQuickRegCode zwraca false dla nieistniejącego kodu autoryzacyjnego
+        $this->assertFalse($this->instance->verifyQuickRegCode('999999'));
+    }
+}
